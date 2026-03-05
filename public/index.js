e760dcf447fd586e9f8cd90ab6b07f199262cfe8
Dongji <tdongji2@gmail.com>
2026-03-05 17:18:22 +0000

    update BASE_URL production

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000" // ✅ URL complète
+const BASE_URL = "https://contact-s5ve.onrender.com" // ✅ URL complète
 const tableBody = document.getElementById("contactTableBody")
 let editId = null
 
